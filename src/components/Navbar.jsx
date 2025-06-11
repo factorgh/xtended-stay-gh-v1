d127af54914b45c0a374506382591359aa5ab615
factorgh <burchellsbale@gmail.com>
2025-06-11 15:15:35 +0000

    UPDte on nmaings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const BookIcon = () => (
 const Navbar = () => {
   const navLinks = [
     { name: "Home", path: "/" },
-    { name: "Hotels", path: "/rooms" },
+    { name: "Rooms", path: "/rooms" },
     { name: "Experience", path: "/experience" },
     { name: "About", path: "/about" },
   ];
